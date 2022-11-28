783856b1d37f2992a56399e697c4e70b80f27083
Lars <lars@lehdev.de>
2022-11-28 22:20:53 +0100

    Added acces to User

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,23 @@
 import { NextPage } from "next";
+import { useSession } from "next-auth/react";
 import React from "react";
+import logo from "../../public/logo.png";
+import Image from "next/image";
 
 const NavBar: NextPage = () => {
-  return <>Test</>;
+  const { data: session, status } = useSession();
+
+  if (session) {
+    if (session.user!.access == true) {
+      return <></>;
+    }
+  }
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
 };
 
 export default NavBar;
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,6 +7,7 @@ declare module "next-auth" {
   interface Session {
     user?: {
       id: string;
+      access: Boolean;
     } & DefaultSession["user"];
   }
 }
