bb9300794ae1a5574de0031139a4156d742b88d5
Lars <lars@lehdev.de>
2022-12-02 20:59:16 +0100

    Added Navbar Component
    - added Flowbite for easier dropdowns
    - added test page for router-tests
    - modified db schema

---
+++ src/pages/twitch/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import React from "react";
+import NavBar from "../../components/navbar";
+
+const TwitchPage: NextPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Twitch - Test</h1>
+    </div>
+  );
+};
+
+export default TwitchPage;
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -8,6 +8,7 @@ declare module "next-auth" {
     user?: {
       id: string;
       access: Boolean;
+      theme: string;
     } & DefaultSession["user"];
   }
 }
