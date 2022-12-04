cf117c06b1589a37db766c24ab471160663cafab
Lars <lars@lehdev.de>
2022-12-04 22:05:04 +0100

    Resolved issue with Loginbutton
    button was nested inside a button, causing a DOM error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -92,7 +92,11 @@ const NavBar: NextPage = () => {
           <Dropdown
             arrowIcon={false}
             inline={true}
-            label={<Button>Login</Button>}
+            label={
+              <div className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
+                Login
+              </div>
+            }
           >
             <Dropdown.Item onClick={() => signIn("discord")}>
               Login with Discord
