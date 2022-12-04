f9e2dbcd0d6ce8899e81d3cbcb8b26061f24091c
Lars <lars@lehdev.de>
2022-12-05 00:24:27 +0100

    added all api-endpoints to trpc

--- src/pages/twitch/index.tsx
+++ src/pages/twitch/index.tsx
@@ -10,8 +10,15 @@ import { trpc } from "../../utils/trpc";
 const TwitchPage: NextPage = () => {
   const { data: session, status } = useSession();
   const channels = trpc.auth.getAllChannels.useQuery();
+  const chnCount = trpc.auth.getChannelMsgCount.useQuery({
+    id: 92108465,
+  });
 
-  if (status === "loading" || channels.status === "loading") {
+  if (
+    status === "loading" ||
+    channels.status === "loading" ||
+    chnCount.status === "loading"
+  ) {
     return (
       <>
         <main>Loading...</main>
@@ -38,6 +45,7 @@ const TwitchPage: NextPage = () => {
       <NavBar></NavBar>
       <h1>Twitch - Test</h1>
       <p>Currently monitoring {chn.length} Channels</p>
+      <p>Message Count of channel with id 92108465: {chnCount.data!.count}</p>
     </div>
   );
 };
---
+++ src/pages/twitch/user/[userId].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserIdPage() {
+  return <div>UserIdPage</div>;
+}
+
+export default UserIdPage;
