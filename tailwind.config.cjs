6f17058509c26c36df795135022438508cecf89f
Lars <lars@lehdev.de>
2022-12-10 17:21:37 +0100

    Fixed Dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite-react/**/*.js",
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  //plugins: [require("flowbite/plugin")],
 };
